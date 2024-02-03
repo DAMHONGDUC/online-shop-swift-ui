860cab2ed0c5886d968111acd3b60e3d3e159dc3
Dam Hong Duc <hongduc001h@gmail.com>
2024-02-03 13:20:23 +0700

    Initial Commit

---
+++ onlineShopSwiftUI/onlineShopSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  onlineShopSwiftUIApp.swift
+//  onlineShopSwiftUI
+//
+//  Created by Hong Duc on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct onlineShopSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
